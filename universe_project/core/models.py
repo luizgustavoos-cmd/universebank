9bdea4fed987ac1fb16637d2e8ca4e135b995af0
luizgustavoos-cmd <luizgustavo20172001@gmail.com>
2025-10-08 01:15:06 -0300

    lapresentação lista ok, criação de index e templates ok

--- universe_project/core/models.py
+++ universe_project/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model): 
+
+    nome = models.CharField(max_length=50)
+    conta = models.CharField(max_length=10, unique=True)
+    saldo = models.DecimalField(max_digits=12, decimal_places=2, default=0.00) 
+    email = models.EmailField(max_length=50, unique=True) 
+    telefone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"Conta {self.conta} - {self.nome}"
--- universe_project/core/views.py
+++ universe_project/core/views.py
@@ -1,5 +1,14 @@
-
-from django.http import HttpResponse
+# core/views.py
+from django.shortcuts import render
+# Lembre-se, use o nome da classe correto: Cliente
+from .models import Cliente 
 
 def index(request):
-    return HttpResponse("UniverseBank: Hello, World!")
+    # CORREÇÃO 1: Precisa do método .all() para buscar todos os objetos
+    clientes = Cliente.objects.all() 
+    
+    # CORREÇÃO 2: A função render() é chamada com parênteses, não chaves.
+    context = {'clientes': clientes}
+    
+    # Renderiza o template 'index.html' passando o dicionário de contexto.
+    return render(request, 'index.html', context)
